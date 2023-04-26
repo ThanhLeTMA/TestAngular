b412c15524f7259ba893cbdc2f2501b8af23e9ca
ThanhLe <lethanh@tma.com.vn>
2023-04-26 16:01:48 +0700

    ng-template

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public name = 'Thanh';
+  public age = 25;
+  public traiCay = []
+  constructor() { }
+  ngOnInit(): void {
+    console.log('trai cay = '+ this.traiCay)
+  }
+
+  public resetName(): void {
+    console.log('resetName');
+    this.name = ' ';
+  }
 
 }
