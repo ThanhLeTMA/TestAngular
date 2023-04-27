f48d17bf82a009d63bacd2508c65b325c672473b
ThanhLe <lethanh@tma.com.vn>
2023-04-27 11:25:45 +0700

    ngFor4

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,17 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   public name = 'Thanh';
   public age = 25;
-  public traiCay = ['táo', 'nho', 'cam', 'quýt']
+  public traiCay = ['táo', 'nho', 'cam', 'quýt', 'ổi'];
+  public traiCay2 = [
+    { ten: 'Táo', gia: 12, haGia: true },
+    { ten: 'Nho', gia: -3, haGia: false },
+    { ten: 'Cam', gia: 435, haGia: false },
+    { ten: 'Quýt', gia: -69, haGia: true },
+    { ten: 'Ôỉ', gia: -547, haGia: false },
+  ];
   constructor() { }
   ngOnInit(): void {
-    console.log('trai cay = '+ this.traiCay)
+    console.log('trai cay = ' + JSON.stringify(this.traiCay2));
   }
-
-  
-
 }
